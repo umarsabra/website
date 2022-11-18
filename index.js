14bf58680ed7be6fc77a955823e359a6148897c1
umarsabra <omaarsaabra@outlook.com>
2022-11-18 18:11:53 +0200

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const next_btn = document.getElementById("btn-next");
+const prev_btn = document.getElementById("btn-prev");
+const slide_container = document.getElementById("slides-container");
+const slides = document.querySelectorAll(".slide");
+
+let counter = 1;
+
+const add_animation = (active_slide) => {
+  if (active_slide == 0) {
+  } else if (active_slide == 5) {
+  } else {
+    console.log(active_slide);
+    slides.forEach((slide) => {
+      slide
+        .querySelector(".slide__text")
+        .classList.remove("slide-text-animation");
+    });
+    slides[active_slide]
+      .querySelector(".slide__text")
+      .classList.add("slide-text-animation");
+  }
+};
+
+next_btn.addEventListener("click", () => {
+  slide_container.style.transition = "all .3s ease-in-out";
+  counter++;
+  add_animation(counter);
+  slide_container.style.transform = `translateX(-${100 * counter}%)`;
+});
+
+prev_btn.addEventListener("click", () => {
+  slide_container.style.transition = "all .3s ease-in-out";
+  counter--;
+  add_animation(counter);
+  slide_container.style.transform = `translateX(-${100 * counter}%)`;
+});
+
+slide_container.addEventListener("transitionend", () => {
+  if (slides[counter].id === "first-clone") {
+    slide_container.style.transition = "none";
+    counter = 1;
+    slide_container.style.transform = `translateX(-${100 * counter}%)`;
+  }
+  if (slides[counter].id === "last-clone") {
+    slide_container.style.transition = "none";
+    counter = slides.length - 2;
+    slide_container.style.transform = `translateX(-${100 * counter}%)`;
+  }
+});
